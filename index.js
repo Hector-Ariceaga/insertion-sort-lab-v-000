1247b413bcbe49855421b7764cb244b8d2db5dfd
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-03-15 17:38:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function findMinAndRemove(array){
-
+  let minimum = array[0]
+  let minIndex = 0
+  for (let i=0; i < array.length; i++) {
+    if (i < minimum) {
+      minimum = array[i]
+    }
+  return minimum
+  }
 }
 
 function insertionSort(array){
